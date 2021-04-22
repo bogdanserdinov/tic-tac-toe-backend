b972fe9aa335f6636bdcd5638e56adbff1a0d101
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-04-22 16:44:41 +0300

    add modules to project

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"awesomeProject/tictactoe_web"
-	"awesomeProject/tictactoe_web/pkg/handler"
+	"github.com/bogdanserdinov/tic-tac-toe-web"
+	"github.com/bogdanserdinov/tic-tac-toe-web/pkg/handler"
 	"log"
 )
 
-func main(){
-	hndl := new(handler.Handler)	//struct, but method InitRoutes == http.Handler
+func main() {
+	hndl := new(handler.Handler) //struct, but method InitRoutes == http.Handler
 
 	server := new(tictactoe_web.Server)
-	if err := server.Run("8080",hndl.InitRoutes());err != nil{
-		log.Fatalln("error occurred while running http server",err.Error())
+	if err := server.Run("8080", hndl.InitRoutes()); err != nil {
+		log.Fatalln("error occurred while running http server", err.Error())
 	}
 }
